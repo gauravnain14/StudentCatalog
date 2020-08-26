f40fe65c6e13a3047e5790c841befe19b452527f
Gaurav <gauravnain14@gmail.com>
2020-08-26 10:49:58 -0400

    second push

--- src/main/java/com/studentcatalog/StudentCatalogApplication.java
+++ src/main/java/com/studentcatalog/StudentCatalogApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentCatalogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentCatalogApplication.class, args);
 	}
-
 }
